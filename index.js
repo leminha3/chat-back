d83acdac96c1482c25c657c173641c333195fc38
minhle <99340541+leminha3@users.noreply.github.com>
2025-10-05 23:33:52 +0700

    l

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ["http://localhost:5500", "http://127.0.0.1:5500"]
+  origin: ["http://localhost:5500", "http://127.0.0.1:5500", "https://chat-front-tvge.onrender.com"]
 }));
 app.use(bodyParser.json());
 
